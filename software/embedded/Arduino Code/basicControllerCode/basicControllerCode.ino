99d93dc2144e18cdc37d55d445bdd8cb4659a3e9
Nick McComb <nick@nickmccomb.net>
2015-01-25 13:10:29 -0800

    Added right channel, enable switch

--- software/embedded/Arduino Code/basicControllerCode/basicControllerCode.ino
+++ software/embedded/Arduino Code/basicControllerCode/basicControllerCode.ino
@@ -12,9 +12,12 @@
 #define CH3_MIN 919
 #define CH3_MAX 1644
 #define CH3_STOP ((CH3_MAX-CH3_MIN)/2 + CH3_MIN)
-#define CH2_MAGNATUDE ((CH3_MAX - CH3_MIN) / 2)
+#define CH3_MAGNATUDE ((CH3_MAX - CH3_MIN) / 2)
 
-#define STOP_CONSTANT 20
+#define CH6_MIN 950   //Higher than actual, use < statement
+#define CH6_MAX 1630  //Lower than actual, use > statement
+
+#define STOP_CONSTANT 30
 
 
 void setup() {
@@ -32,25 +35,59 @@ void loop() {
   int lChannel = pulseIn(CHANNEL2, HIGH);
   lChannel -= CH2_STOP;
   
-  float ratio;
+  int rChannel = pulseIn(CHANNEL3, HIGH);
+  rChannel -= CH3_STOP;
   
-  Serial.print("Left:  ");
-  if(abs(lChannel) > STOP_CONSTANT){  //Moving case
-	ratio = ((float)lChannel / (float)CH2_MAGNATUDE);
-	Serial.print(abs(ratio) * 100);
-	Serial.print("% ");
-	if(lChannel > 0){
-		Serial.println("forward");
-	}
-	else {
-		Serial.println("backward");
-	}
+  int enChannel = pulseIn(CHANNEL6, HIGH);
+  
+  float lRatio;
+  float rRatio;
+  
+  if(enChannel > CH6_MAX){  //Motors are good to go to be operated in a normal fashion
+	  Serial.println("MOTOR GO!");
+	  
+	  Serial.print("Left:  ");
+	  if(abs(lChannel) > STOP_CONSTANT){  //Moving case
+		lRatio = constrain(((float)lChannel / (float)CH2_MAGNATUDE), -1, 1);
+		Serial.print(abs(lRatio) * 100);
+		Serial.print("% ");
+		if(lChannel > 0){
+			Serial.println("forward");
+		}
+		else {
+			Serial.println("backward");
+		}
+	  }
+	  else{  //Not moving case
+		  Serial.println("Stop");
+	  }
+	  
+	  
+	  Serial.print("Right: ");
+	  if(abs(rChannel) > STOP_CONSTANT){  //Moving case
+		rRatio = constrain(((float)rChannel / (float)CH3_MAGNATUDE), -1, 1);
+		Serial.print(abs(rRatio) * 100);
+		Serial.print("% ");
+		if(rChannel > 0){
+			Serial.println("forward");
+		}
+		else {
+			Serial.println("backward");
+		}
+	  }
+	  else{  //Not moving case
+		  Serial.println("Stop");
+	  }
   }
-  else{  //Not moving case
-	  Serial.println("Stop");
+  else {  //All motor stop
+	  Serial.println("NO MOTOR :(");
   }
   
+  /*
   
+
+  
+  */
   
   delay(100);
   //if(lChannel )
