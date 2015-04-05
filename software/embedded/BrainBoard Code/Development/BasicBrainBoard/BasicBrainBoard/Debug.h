a8b3befdac4b87729863bf3ec693e9fc735a88fb
Nick Ames <nick@fetchmodus.org>
2015-04-05 01:17:55 -0700

    Deleted object files.

--- software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Debug.h
+++ software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Debug.h
@@ -3,14 +3,15 @@
  *
  * Created: 3/5/2015 8:52:15 AM
  *  Author: N/A
- */ 
-
-
+ */
 #ifndef DEBUG_H_
 #define DEBUG_H_
 
 #include "XMegaLib.h"
 
+/* Debug message function. When called, the resulting string
+ * is sent up the wire to the control computer, where it will
+ * (hopefully) be logged in a text file. */
 
 
 #endif /* DEBUG_H_ */
