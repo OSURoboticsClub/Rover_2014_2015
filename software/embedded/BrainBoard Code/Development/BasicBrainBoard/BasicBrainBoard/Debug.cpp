161cb0e2c6e1647f15e1a651a48c832fc27642fa
Nick Ames <nick@fetchmodus.org>
2015-04-05 01:33:12 -0700

    Wrote out Debug.h prototypes.

--- software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Debug.cpp
+++ software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Debug.cpp
@@ -6,3 +6,18 @@
  */ 
 
 #include "Debug.h"
+
+/* Debug message functions. When called, the resulting string
+ * is sent up the wire to the control computer, where it will
+ * (hopefully) be logged in a text file.
+ * Arguments and functionality are the same as printf(). */
+void debug_msg(const char *format, ...){
+	//TODO
+}
+
+/* Debug function for use with format strings in FLASH memory.
+ * See http://www.nongnu.org/avr-libc/user-manual/pgmspace.html
+ * to learn why this is a good idea! */
+void debug_msg_p(const char *progmem_format, ...){
+	//TODO
+}
--- software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Debug.h
+++ software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Debug.h
@@ -9,9 +9,15 @@
 
 #include "XMegaLib.h"
 
-/* Debug message function. When called, the resulting string
+/* Debug message functions. When called, the resulting string
  * is sent up the wire to the control computer, where it will
- * (hopefully) be logged in a text file. */
+ * (hopefully) be logged in a text file.
+ * Arguments and functionality are the same as printf(). */
+void debug_msg(const char *format, ...);
 
+/* Debug function for use with format strings in FLASH memory.
+ * See http://www.nongnu.org/avr-libc/user-manual/pgmspace.html
+ * to learn why this is a good idea! */
+void debug_msg_p(const char *progmem_format, ...);
 
 #endif /* DEBUG_H_ */
