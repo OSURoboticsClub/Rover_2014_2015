7d8a1c21ef8c8c327865817db0c5444f18a7b104
Nick McComb <nrpickle@gmail.com>
2015-05-01 23:17:42 -0700

    Kangaroo working a little better

--- software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Drive.cpp
+++ software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Drive.cpp
@@ -42,30 +42,32 @@ void driveMain(){
 		}
 	}
 	*/
-		
-	SendStringSABER_UNO("1,start\n");
+	
+	_delay_ms(2000);
+	
 	SendStringSABER_UNO("2,start\n");
-	SendStringPC("1,start\r\n");
+	//SendStringSABER_UNO("2,start\n");
+	SendStringPC("2,start\r\n");
 	_delay_ms(1000);
 
 	//Set the units for the encoder. The values entered are rough and might
 	//need to be tuned. Calculated from the website. 
-	SendStringSABER_UNO("1, UNITS 917 cm = 270000 lines\n"); //check out exact values
-	_delay_ms(10);
 	SendStringSABER_UNO("2, UNITS 917 cm = 270000 lines\n"); //check out exact values
+	_delay_ms(10);
+	//SendStringSABER_UNO("2, UNITS 917 cm = 270000 lines\n"); //check out exact values
 	
-	SendStringPC("1, UNITS 917 cm = 270000 lines\r\n");
+	SendStringPC("2, UNITS 917 cm = 270000 lines\r\n");
 	
 	//SendStringSABER_UNO("UNITS 917 cm = 270000 lines\n");
-	//SendStringSABER_UNO("lines \n");z
+	//SendStringSABER_UNO("lines \n");
 
 	_delay_ms(1000);
 	
 	while (1)
 	{
-		SendStringSABER_UNO("1, s50\n");  //Set the speed to 50 cm/s
-		SendStringPC("1, s50\r\n");
-		_delay_ms(10);
+		SendStringSABER_UNO("2, s50\n");  //Set the speed to 50 cm/s
+		SendStringPC("2, s50\r\n");
+		_delay_ms(100);
 	}
 	
 	
@@ -100,7 +102,7 @@ void SendStringSABER_UNO(char *present){
 	for(int i = 0 ; present[i] != '\0' ; i++){
 		while(!USART_IsTXDataRegisterEmpty(&USARTE0));
 		USART_PutChar(&USARTE0, present[i]);
-		_delay_us(50);  //DEGBUGGING
+		_delay_us(500);  //DEGBUGGING
 	}
 }
 
