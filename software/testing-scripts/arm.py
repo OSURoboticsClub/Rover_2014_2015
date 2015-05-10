80fdec296e52604b771c965b985c769284123335
Nick <nick@nickmccomb.net>
2015-05-10 16:36:50 -0700

    Functional communication, need to remove constantly homing

--- software/testing-scripts/arm.py
+++ software/testing-scripts/arm.py
@@ -25,9 +25,11 @@ class SerialBoard(object):
                 if device_id.strip() == self.id_str.strip():
                     s.write("r")
                     self.serial = s
+                    print "# Found the correct board #"
                     return "test"
             except (OSError, serial.SerialException):
                 pass
+        print "##! Cannot find serial port... !##"
     
     def write_packet(self):
         pass
@@ -39,7 +41,7 @@ class ArmSerial(SerialBoard):
         self.timeout = 3
         self.id_str = "ARM"
         self.serial = None;
-        self.packet_struct = "ccccccc"
+        self.packet_struct = "cccccccc"
         self.x = 0
         self.y = 0
         self.z = 0
@@ -69,7 +71,7 @@ class ArmSerial(SerialBoard):
             command = self.command
         else:
             self.command = command
-        packet = struct.pack(self.packet_struct, chr(0xff), chr(command), chr(x), chr(y), chr(z), chr(self.checksum()), chr(0xff))
+        packet = struct.pack(self.packet_struct, chr(0xff), chr(command), chr(x), chr(y), chr(z), chr(0), chr(self.checksum()), chr(0xff))
         self.serial.write(packet)
         
 class ArmController(object):
@@ -78,11 +80,11 @@ class ArmController(object):
         self.serial = ArmSerial();
         if(not self.serial.find_port()):
             raise Exception("Arm controler could not be started; Serial connection failed.")
-        self.cycle_size = .02
+        self.cycle_size = 1
         self.x = 0
         self.y = 0
         self.z = 0
-        self.command = 0
+        self.command = 0x04
     
     
     def move(self, x, y, z, grip):
@@ -94,12 +96,12 @@ class ArmController(object):
         
         if 0 > z > 255:
             raise Exception("z not in range [0, 255]")
-        if grip > 1:
-            raise Exception("grip must be 1 or 0")
+#        if grip > 1:
+#            raise Exception("grip must be 1 or 0")
         self.x = x
         self.y = y
         self.z = z
-        self.command = grip
+#        self.command = grip
     
     #
     # Loop to be constantly executed to keep sending packets and to perform
@@ -108,6 +110,7 @@ class ArmController(object):
     def loop(self):
         while True:
             self.serial.write_packet(self.command, self.x, self.y, self.z)
+            print("Wrote packet, command = " + str(self.command) )
             time.sleep(self.cycle_size)
     
 
@@ -115,14 +118,15 @@ if __name__ == '__main__':
   arm = ArmController()
   loop = threading.Thread(target=arm.loop)
   loop.start()
+
+  firstLoop = 1
  
   while(1):
     x = int(raw_input("x:"))
     y = int(raw_input("y:"))
     z = int(raw_input("z:"))
-    grip = int(raw_input("grip([0|1]):"))
     try:
-        arm.move(x, y, z, grip)
+        arm.move(x, y, z, 0x04)
     except Exception, e:
         print e
  
