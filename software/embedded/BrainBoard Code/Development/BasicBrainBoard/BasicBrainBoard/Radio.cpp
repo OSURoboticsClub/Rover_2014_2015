a665f38aaf9ca0e75b7436e00ee8828e89f779b5
Nick McComb <nrpickle@gmail.com>
2015-05-29 18:30:59 -0700

    Added basic radio definitions

--- software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Radio.cpp
+++ software/embedded/BrainBoard Code/Development/BasicBrainBoard/BasicBrainBoard/Radio.cpp
@@ -2,12 +2,10 @@
  * Radio.cpp
  *
  * Created: 3/4/2015 1:07:41 AM
- *  Author: nrpic_000
+ *  Author: Nick McComb
  */ 
 
 #include "Radio.h"
-
-//Hai
 
 /*
 Description: This function holds all of the code for the radio firmware for the BB
@@ -26,6 +24,42 @@ void radioMain(){
 }
 
 
+/********** 
+
+Pin definitions:
+
+Drive Board Connector
+Pause - PF5
+Drive_RX - PF2
+Drive_TX - PF3
+
+SmartFinder Connector
+SmartFinder_RX - PE3
+SmartFinder_TX - PE2
+
+Stepper Driver
+!Fault - PB3
+Step - PE4
+Direction - PE7
+Enable - PE5
+
+Limit Switch 
+PF6
+
+**********/ 
+
 void radioInit(){
+	//Setup pause input
+	PORTF.DIRCLR = (PIN5_bm);
+	
+	//Setup Stepper Driver
+	PORTB.DIRCLR = (PIN3_bm);  //nFault
+	PORTE.DIRSET = (PIN4_bm);  //Step
+	PORTE.DIRSET = (PIN7_bm);  //Direction
+	PORTE.DIRSET = (PIN5_bm);  //Enable
+	
+	//Setup Limit Switch
+	PORTF.DIRCLR = (PIN6_bm);
+	
 	//Potato!
 }
