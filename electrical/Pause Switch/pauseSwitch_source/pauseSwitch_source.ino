26d15f6c3f50c5e826f168dacd97cb9f3a9d6971
Nick McComb <mccombn@onid.oregonstate.edu>
2015-06-04 15:20:31 -0700

    Updated pause switch code

--- electrical/Pause Switch/pauseSwitch_source/pauseSwitch_source.ino
+++ electrical/Pause Switch/pauseSwitch_source/pauseSwitch_source.ino
@@ -10,17 +10,17 @@
 ********************************************************************************/
 
 //Define macros for I/O
-#define PAUSE_BUTTON 11	        //Pause Button
-#define NPAUSE_BUTTON 9         //Unpause Button
-#define STATUS_LED 12      	//Status LED
-#define PWR_LED 8               //Power LED
+#define PAUSE_BUTTON 5	        //Pause Button
+#define NPAUSE_BUTTON 6         //Unpause Button
+#define STATUS_LED 3      	//Status LED
+#define PWR_LED 2               //Power LED
 #define PWR A0                  //Battery Voltage Input
-#define XB 3			//XBee output
+#define XB 4			//XBee output
 
 //Define macros for options
 #define DEF_STATE 0 		//Default start state, 0 for pause/1 for run
 #define BLINK_INTERVAL 500      //Delay between blinks of status led when paused
-#define LOW_POWER_BLINK 100     //Delay between blinks of all LEDs when power is low
+#define LOW_POWER_BLINK 150     //Delay between blinks of all LEDs when power is low
 
 /*******************************************************************************
 *				 CODE BODY                                     *
@@ -28,50 +28,60 @@
 
 //Set Variables to Options
 int runningState = DEF_STATE;
-int ledState = 1;
-int batteryState;
+int ledState = 0;
+int battFlag = 0;
 long currMillis = 0;
 long prevMillis = 0;
 
 void setup() {
   //Serial.begin(9600);
-  
+
   //Set inputs
   pinMode(PAUSE_BUTTON, INPUT);
   pinMode(NPAUSE_BUTTON, INPUT);
-  
+
   //Set outputs
   pinMode(STATUS_LED, OUTPUT);
   pinMode(PWR_LED, OUTPUT);
   pinMode(XB, OUTPUT);
-  
+
   //Write default state to XBee
   digitalWrite(XB, DEF_STATE);
-  
+
   //Turn on Power LED
   digitalWrite(PWR_LED, HIGH);
-  
+
   //Turn the Status LED on if the default state is pause
-  if(DEF_STATE == 0)
+  if (DEF_STATE == 0)
     digitalWrite(STATUS_LED, HIGH);
 }
 
 void loop() {
-  if(digitalRead(PWR) < 625){
-     currMillis = millis();
-     if(currMillis - prevMillis > LOW_POWER_BLINK){
-       prevMillis = currMillis;
-      
-       if(ledState == LOW)
-         ledState = HIGH;
-       else if(ledState == HIGH)
-         ledState = LOW;
-        
-       digitalWrite(STATUS_LED, ledState);
-       digitalWrite(PWR_LED, ledState);
-     }
+  delay(1);
+  //Serial.println(analogRead(PWR));
+  if (analogRead(PWR) < 600) {
+    battFlag = 1;
   }
-  else{
+  else if (analogRead(PWR) > 650) {
+    battFlag = 0;
+  }
+
+  if (battFlag == 1) {
+    currMillis = millis();
+    if (currMillis - prevMillis > LOW_POWER_BLINK) {
+      prevMillis = currMillis;
+
+      if (ledState == LOW)
+        ledState = HIGH;
+      else if (ledState == HIGH)
+        ledState = LOW;
+
+      digitalWrite(STATUS_LED, ledState);
+      digitalWrite(PWR_LED, ledState);
+    }
+  }
+  else if (battFlag == 0) {
+    digitalWrite(PWR_LED, HIGH);
     //Read button and swap state
     if ((digitalRead(PAUSE_BUTTON) == HIGH) && (runningState == 1)) {
       runningState = 0;
@@ -82,20 +92,25 @@ void loop() {
     else if ((digitalRead(NPAUSE_BUTTON) == HIGH) && (runningState == 0)) {
       runningState = 1;
       digitalWrite(XB, HIGH);
+      digitalWrite(STATUS_LED, LOW);
       delay(250);
     }
-    
+
     //Change state of LED every BLINK_INTERVAL
     currMillis = millis();
-    if ((currMillis - prevMillis > BLINK_INTERVAL) && (runningState == 1)) {
+    if (currMillis - prevMillis > BLINK_INTERVAL) {
       prevMillis = currMillis;
-      
-      if(ledState == LOW)
-        ledState = HIGH;
-      else
-        ledState = LOW;
-        
-      digitalWrite(STATUS_LED, ledState);
+      if (runningState == 1) {
+        if (ledState == LOW)
+          ledState = HIGH;
+        else
+          ledState = LOW;
+
+        digitalWrite(STATUS_LED, ledState);
+      }
+      else if (runningState == 0) {
+        digitalWrite(STATUS_LED, HIGH);
+      }
     }
   }
 }
